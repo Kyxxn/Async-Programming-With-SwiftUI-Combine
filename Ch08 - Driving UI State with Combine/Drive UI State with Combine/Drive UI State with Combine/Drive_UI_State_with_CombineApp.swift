118242a8ff7b612ec24687d4e28b98574bb294da
박효준 <gywns626@naver.com>
2025-06-22 14:43:27 +0900

    feat: Chapter 08 - Driving UI State with Combine

---
+++ Ch08 - Driving UI State with Combine/Drive UI State with Combine/Drive UI State with Combine/Drive_UI_State_with_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_UI_State_with_CombineApp.swift
+//  Drive UI State with Combine
+//
+//  Created by 박효준 on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Drive_UI_State_with_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SignUpForm()
+        }
+    }
+}
