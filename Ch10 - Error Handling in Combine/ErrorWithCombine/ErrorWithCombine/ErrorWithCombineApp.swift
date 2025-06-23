f7266093000f254968659a73799c000a15bee20c
박효준 <gywns626@naver.com>
2025-06-23 21:40:47 +0900

    feat: Chapter 10 - Error Handling in Combine 템플릿

---
+++ Ch10 - Error Handling in Combine/ErrorWithCombine/ErrorWithCombine/ErrorWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorWithCombineApp.swift
+//  ErrorWithCombine
+//
+//  Created by 박효준 on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ErrorWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
